9cdd3196922ca4ab7f9b49799377a63bd5fe819e
mokaim <mokaim@outlook.com>
2023-10-29 23:28:17 +0900

    React 에 Foundation 트리거 위한 Jquery 적용완료. MotionUI 테스트 완료!!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 
-import 'jquery/dist/jquery.min'
 import 'what-input/dist/what-input.min'
+import 'foundation-sites/dist/js/foundation.min'
 
-import './custom-style/scss/includes.scss'
-import 'foundation-sites/dist/css/foundation-float.min.css'
+//import './custom-style/scss/includes.scss'
 import './custom-style/main.css'
 import './custom-style/main'
-
-
 
 
 import Background from './components/Background'
 
 
+const Jquery = require('jquery')
+
 function App() {
+
+    Jquery(document).ready($ => {$(document).foundation()})
 
   return (
     <div className="App">
