3d47ef1bec73079a0f9de66a5a205d87eb031fab
mokaim <mokaim@outlook.com>
2023-10-30 23:02:49 +0900

    react-router-dom 추가

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default function SideNav(props) {
     return (
         <aside>
             <div className="sidebar" ref={props.forwardedRef}>
-                <a className="active" href={"test"}>Home</a>
+                <a  href={"test"}>Home</a>
                 <a href={"test"}>News</a>
                 <a href={"test"}>Contact</a>
                 <a href={"test"}>About</a>
