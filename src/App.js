1f0fa97d32a9f6e7a93bb895eaa329c407a2e364
mokaim <mokaim@outlook.com>
2023-11-03 03:02:40 +0900

    필요없는 import 모듈 삭제

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import './App.css';
 
 import 'what-input/dist/what-input.min'
 import 'foundation-sites/dist/js/foundation.min'
-import 'motion-ui/dist/motion-ui.min'
-
-//import './custom-style/scss/includes.scss'
 import './custom-style/main.css'
 import './custom-style/main'
-import './custom-style/scss/animation.scss'
 
 import Background from './components/Background'
 import {redirect} from "react-router-dom";
