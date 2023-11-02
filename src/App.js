60dfab25bc7f4d7eb7f8c48e9b2d854ff543ccf9
mokaim <mokaim@outlook.com>
2023-11-03 02:53:58 +0900

    motion-ui 재적용 테스트완료

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import './App.css';
 
 import 'what-input/dist/what-input.min'
 import 'foundation-sites/dist/js/foundation.min'
+import 'motion-ui/dist/motion-ui.min'
 
 //import './custom-style/scss/includes.scss'
 import './custom-style/main.css'
 import './custom-style/main'
-
+import './custom-style/scss/animation.scss'
 
 import Background from './components/Background'
 import {redirect} from "react-router-dom";
 
 export async function loader() {
-    console.log('tes : ' + window.location.pathname)
     if (window.location.pathname === "/") {
         return redirect(`/home`)
     }
 }
 
-
-
-const Jquery = require('jquery')
-
 function App() {
+
+    const Jquery = require('jquery')
 
     Jquery(document).ready($ => {
         $(document).foundation()
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -12,7 +12,7 @@ export default function SideNav(props) {
     console.log('params : ' + JSON.stringify(params))
     return (
         <aside>
-            <div className="sidebar" ref={props.forwardedRef}>
+            <div className="sidebar" ref={props.forwardedRef} id={"responsive-menu"} data-toggler={""} data-animate="fade-in fade-out">
                 <NavLink to={"/home"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>Home</NavLink>
                 <NavLink to={"/news"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>News</NavLink>
                 <NavLink to={"/contact"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>Contact</NavLink>
